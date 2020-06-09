ceaa87ea2905b7d6f28f09521439ed487b4c648f
Heraldo araujo <heraldo@live.com>
2020-06-09 09:23:44 -0300

    Update gera_programa.sql

--- gera_programa.sql
+++ gera_programa.sql
@@ -1,5 +1,5 @@
 REM Autor            : Heraldo Silva
-REM Data atualizacao : 28/01/2019
+REM Data atualizacao : 09/06/2020
 REM Descricao        : Gera o DDL de um programa PLSQL e joga a saida em um spool.
 REM Nota 1           : Deve passar como parametro o <dono>.<objeto>
 REM Exemplo          : SQL> @gera_program.sql "dono.objeto";
