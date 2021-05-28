72adba9d44ea998fbd3c2ceb4ee75073945ba1df
heraldoaraujo <heraldo@live.com>
2021-05-28 15:36:51 -0300

    atualizando arquivo gera_programa

--- gera_programa.sql
+++ gera_programa.sql
@@ -1,26 +1,31 @@
 REM Autor            : Heraldo Silva
-REM Data atualizacao : 09/06/2020
+REM Data atualizacao : 28/05/2021
 REM Descricao        : Gera o DDL de um programa PLSQL e joga a saida em um spool.
 REM Nota 1           : Deve passar como parametro o <dono>.<objeto>
-REM Exemplo          : SQL> @gera_program.sql "dono.objeto";
+REM Exemplo          : SQL> @gera_program.sql "dono-objeto";
 
 prompt G e r a   P r o g r a m a   P L / S Q L 
 
-set timing   off;
-set head     off;
-set feedback off;
-set verify   off;
+set line 200
+set timing    off;
+set head      off;
+set feedback  off;
+set verify    off;
+set trimspool on;
 
-spool        ddl_programa.sql;
+define entrada_objeto = '&1';
+spool  &&ENTRADA_OBJETO
 
 select nvl(text,'PROGRAMA NAO ENCONTRADO')
 from dba_source 
-where owner||'.'||name = upper('&1')
+where owner||'.'||name = replace(upper('&1'),'-','.')
 ORDER BY type asc, line ASC;
 
 spool        off;
 
-set verify   on;
-set timing   on;
-set head     on;
-set feedback on;
+set verify    on;
+set timing    on;
+set head      on;
+set feedback  on;
+set trimspool off;
+
